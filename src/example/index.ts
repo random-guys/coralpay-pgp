184eb05b72671ab933944612b5108e13ebf41b96
Raymond Tukpe <rtukpe@gmail.com>
2019-11-08 10:42:09 +0100

    feat: updates sample.

--- src/example/index.ts
+++ src/example/index.ts
@@ -1,17 +1,34 @@
 import { join } from 'path';
 import uuid from 'uuid/v4';
 import { Pgp } from '..';
+import { config } from 'dotenv';
+import { openSync, writeFileSync, closeSync } from 'fs';
 
 const run = async () => {
+  config();
+
   try {
-    const passphrase = '5wanp6eegH';
-    const publicKeyPath = join(__dirname, './keys/public_key.txt');
-    const privateKeyPath = join(__dirname, './keys/private_key.txt');
+    // create key files
+    const public_key_file_path = join(__dirname, './public_key.txt');
+    const private_key_file_path = join(__dirname, './private_key.txt');
+    closeSync(openSync(public_key_file_path, 'w'));
+    closeSync(openSync(private_key_file_path, 'w'));
+
+    // load content from env var
+    const passphrase = process.env.PASSPHRASE;
+    const publicKeyBuffer = Buffer.from(process.env.PUBLIC_KEY, 'base64');
+    const privateKeyBuffer = Buffer.from(process.env.PRIVATE_KEY, 'base64');
+
+    const publicKeyPath = join(__dirname, './public_key.txt');
+    const privateKeyPath = join(__dirname, './private_key.txt');
+
+    writeFileSync(publicKeyPath, publicKeyBuffer.toString('ascii'));
+    writeFileSync(privateKeyPath, privateKeyBuffer.toString('ascii'));
 
     const pgp = new Pgp({
       privateKeyPath,
       publicKeyPath,
-      passphrase,
+      passphrase
     });
 
     const payload = {
